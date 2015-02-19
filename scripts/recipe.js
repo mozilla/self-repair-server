247cb4b56b93b68a2f74a1e0d20d78bdbad6f940
Gregg Lind <gregg.lind@gmail.com>
2015-02-19 15:04:42 -0600

    more wip.  single action commands

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,26 +1,63 @@
 #!/usr/bin/env node
 
 require('shelljs/global');
+config.fatal = true;
 
-console.log(process.argv);
+//require('shelljs/make');  // just as easy to shim!
 
-var which = process.argv[1];
-var recipe = process.argv[2];
+var args = process.argv.slice(2);
+var target = {};
 
-// is this just re-writing npm in order to get out of
-switch (which) {
-  case 'new': {
-    exec('npm run recipe:new', which, {silent:true});
-    break;
+var help = function () {
+  console.log("== Available targets == \n");
+  Object.keys(target).sort().forEach(function (k) {
+    console.log(k,'\n  -', target[k].doc || k);
+  });
+  console.log("\nhelp", "\n  - print help on all targets");
+  exit(0);
+};
+
+function runSingleCommand (args) {
+  var which = args[0];
+  var rest = args.slice(1);
+
+  if (args.length === 0) {
+    help();
   }
-  case 'open': {
-    break;
-  }
-  case 'destroy': {
 
-    break;
+  switch (which) {
+    case "--help":
+    case "help":
+      help();
+      break;
+    default:
+      if (which in target) {
+        target[which].call(null, rest);
+      } else {
+        console.error(which, "not available.  --help for list");
+        exit(1);
+      }
   }
-  default:
-
-    break;
 }
+
+// from shelljs/make but 'one command style'
+setTimeout(function () {runSingleCommand(args);}, 0);
+
+
+
+var info = function (which) {
+  console.log(which, process.argv);
+};
+
+
+target['new'] = function () {
+  info('new');
+};
+target['new'].doc = "make a new recipe";
+
+target.open = function () {
+  info('open');
+};
+//target['new'].doc = "make a new recipe";
+
+
