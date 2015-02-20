d5691269cbd8d1305c0cd00bcac3b51697cb2be9
Gregg Lind <gregg.lind@gmail.com>
2015-02-20 10:22:29 -0600

    Fix #42 integration tests (built file tests)
    - changed index.js not 'automagically run'
    - now up to the index.html file to actually "fire".
    - improved karma confs to know about Travis.

---
+++ test-built/test-built-exports.js
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*jshint forin:true, noarg:false, noempty:true, eqeqeq:true, bitwise:true,
+  strict:true, undef:true, curly:false, browser:true,
+  unused:true,
+  indent:2, maxerr:50, devel:true, node:true, boss:true, white:true,
+  globalstrict:true, nomen:false, newcap:true, esnext: true, moz: true  */
+
+/*global describe, it, require, exports, log */
+
+"use strict";
+
+/* this code mostly exists
+   - for coverage sake
+   - catch when new actions are added
+*/
+let { expect } = require("chai");
+
+describe("built file exports", function () {
+  let heartbeat = window.heartbeat;
+  let expected = {
+    'actions': 'object',
+    'runner': 'object',
+    'personinfo': 'object',
+    'recipes': 'array',
+    'events': 'object',
+    'main': 'function'
+  };
+  it("exports names are right", function () {
+    expect(heartbeat).to.exist();
+    expect(heartbeat).to.have.keys(Object.keys(expected));
+  });
+  it("types are right", function () {
+    for (let k in expected) {
+      let mytype = expected[k]
+      expect(heartbeat[k]).to.be.a(mytype);
+    }
+  });
+});
